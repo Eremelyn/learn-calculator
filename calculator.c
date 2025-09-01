27e0bfa11409d4557fb32af79a03fd26b48c3e7f
Eremelyn <rbecketd@gmail.com>
2025-09-01 15:05:08 -0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -2,22 +2,50 @@
 #include <math.h>
 
 int main(){
+    // Making my operand and operation variables //
     char operand;
+    char num1;
+    char num2;
+    // Asking for the operand //
+
     printf("Enter an Operand (+, -, *, /)");
 
+    // Reading results //
 
-// Math Section//
+    scanf("%c", &operand);
+
+    // Asking the values //
+
+    printf("What numbers you mathin'?");
     
+    // Reading Numbers //
+    scanf("%lf %lf",&num1,&num2);
 
-    switch (operand) {
-        case +:
+    // Math Section//
 
-        
-        case -:
+    switch (operand){
+        case '+':
+            printf("%lf + %lf = %lf", num1,num2,(num1 + num2));
 
-        case *:
+            break;
+        case '-':
+            printf("%lf - %lf = %lf", num1,num2,(num1 - num2));
 
-        case /:
+            break;
+
+        case '*':
+            printf("%lf * %lf = %lf", num1,num2,(num1 * num2));
+
+            break;
+
+        case '/':
+            printf("%lf / %lf = %lf", num1,num2,(num1 / num2));
+            
+            break;
+
 
 
     }
+
+
+}
